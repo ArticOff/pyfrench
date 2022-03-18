a1fc34754975bf8afae257f0cc14a332cb17999f
Artic <81034458+ArticOff@users.noreply.github.com>
2022-03-18 17:59:32 +0100

    Delete pyfrench directory

--- pyfrench/terminal.py
+++
@@ -1,6 +0,0 @@
-from os import *
-from pyfrench import couleur, afficher
-
-def write(commande: str):
-    afficher(couleur.GRAS + couleur.VERT + "[pyfrench]" + couleur.FIN + couleur.GRAS + " -> {}".format(commande) + couleur.FIN)
-    return system(commande)
